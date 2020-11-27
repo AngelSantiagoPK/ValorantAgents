936c6b6e05260d9146969bf609b8097d409693dd
Angel Alexis Santiago <pkninja@Angels-MBP.fios-router.home>
2020-11-26 19:59:09 -0500

    added agent bios prototype

---
+++ src/models/bios.ts
@@ -0,0 +1,9 @@
+import { Bio } from './bio';
+
+export const BIOS: Bio[] = [
+{
+    id: 1,
+    bio: "Following the mysterious events of 'the first light', people with extraordinary abilities, known as Radiants. A shadow organization creates the Valorant Protocol, to fight against future threats."
+}
+
+];
