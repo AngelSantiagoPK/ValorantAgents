ec126c72602e6e2ea0d6d34f2d5d8dff4308721e
Angel Alexis Santiago <pkninja@Angels-MBP.fios-router.home>
2020-12-09 22:08:00 -0500

    update bios

--- src/models/bios.ts
+++ src/models/bios.ts
@@ -3,7 +3,10 @@ import { Bio } from './bio';
 export const BIOS: Bio[] = [
 {
     id: 1,
-    bio: "Following the mysterious events of 'the first light', people with extraordinary abilities, known as Radiants. A shadow organization creates the Valorant Protocol, to fight against future threats."
+    bio: "Following the mysterious event known as 'the first light', special individuals known as Radiants appear around the globe."+
+    "All around the world, rifts to a parallel dimension are torn open."+
+    "A shadow organization creates the Valorant Protocol to combat these and future disasters."+
+    "The Valorant Protocol recruits only the most talented Radiants from around the world."
 }
 
 ];
