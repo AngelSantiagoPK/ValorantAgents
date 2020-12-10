414c0b0b5d32c3188a319008cc726833f845b213
Angel Alexis Santiago <pkninja@Angels-MBP.fios-router.home>
2020-12-09 22:16:17 -0500

    woops, forgot to test the new bios.

--- src/models/bios.ts
+++ src/models/bios.ts
@@ -3,10 +3,10 @@ import { Bio } from './bio';
 export const BIOS: Bio[] = [
 {
     id: 1,
-    bio: "Following the mysterious event known as 'the first light', special individuals known as Radiants appear around the globe."+
-    "All around the world, rifts to a parallel dimension are torn open."+
-    "A shadow organization creates the Valorant Protocol to combat these and future disasters."+
-    "The Valorant Protocol recruits only the most talented Radiants from around the world."
+    bio: "Following the mysterious event known as 'the first light', special individuals known as Radiants appear around the globe, "+
+    "along with rifts to parallel dimensions. "+
+    "A shadow organization creates the Valorant Protocol to combat these and future disasters. "+
+    "The Valorant Protocol recruits only the most talented Radiants from around the world. "
 }
 
 ];
